77dc17c1d67f195df14e438a111748ad3ac1ffce
arianebercu <82817493+arianebercu@users.noreply.github.com>
2025-07-07 13:59:01 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -16,12 +16,11 @@
 
 extern void F77_NAME(idmlikelihood)(double *,int *, int *, double *,int*, double *,double *,double *,
               int *,int *,int *,int *, int *, double *,double*, double *,
-              int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
-
+              int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,int *,double *);
 
 extern void F77_NAME(idmlikelihoodweib)(double *,int *, int *, double *,int*,
               int *,int *, double *,double*, double *,
-              int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
+              int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,int *,double *);
 
 
 extern void F77_NAME(derivaweib)(double *,int *, int *, double *,int*,
@@ -43,6 +42,7 @@ extern void F77_NAME(derivaweiballparadiag)(double *,int *, int *, double *,int*
 extern void F77_NAME(derivaweiballparafirstderiv)(double *,int *, int *, double *,int*,
               int *,int *, double *,double*, double *,
               int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
+
 
 extern void F77_NAME(derivaweibfirstderiv)(double *,int *, int *, double *,int*,
                 int *,int *, double *,double*, double *,
@@ -69,7 +69,6 @@ extern void F77_NAME(firstderivaspline)(double *,int *, int *, double *,int*, do
                      int *,int *,int *,int *, int *, double *,double*, double *,
                      int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
 
-
  extern void F77_NAME(derivasplinesfirstderiv)(double *,int *, int *, double *,int*, double *,double *,double *,
                         int *,int *,int *,int *, int *, double *,double*, double *,
                         int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
@@ -84,8 +83,8 @@ extern void F77_NAME(derivasplinessecondderiv)(double *,int *, int *, double *,i
    
 
 static const R_FortranMethodDef FortranEntries[] = {
-    {"idmlikelihood",(DL_FUNC) &F77_NAME(idmlikelihood),    28},
-    {"idmlikelihoodweib",(DL_FUNC) &F77_NAME(idmlikelihoodweib),    22},
+    {"idmlikelihood",(DL_FUNC) &F77_NAME(idmlikelihood),    29},
+    {"idmlikelihoodweib",(DL_FUNC) &F77_NAME(idmlikelihoodweib),    23},
     {"causalidmlikelihoodweib",(DL_FUNC) &F77_NAME(causalidmlikelihoodweib),    24},
     {"derivaweib",(DL_FUNC) &F77_NAME(derivaweib),    22},
     {"derivaweiballpara",(DL_FUNC) &F77_NAME(derivaweiballpara),    22},
