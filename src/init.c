a1fddf287c8cea1d14378f625936e46ea8902087
arianebercu <82817493+arianebercu@users.noreply.github.com>
2025-07-08 10:39:31 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -61,18 +61,40 @@ extern void F77_NAME(firstderivaspline)(double *,int *, int *, double *,int*, do
                      int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
 
 
+extern void F77_NAME(derivaweibfirstderiv)(double *,int *, int *, double *,int*,
+                int *,int *, double *,double*, double *,
+                int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
+
+ extern void F77_NAME(derivasplinesfirstderiv)(double *,int *, int *, double *,int*, double *,double *,double *,
+                        int *,int *,int *,int *, int *, double *,double*, double *,
+                        int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
+
+extern void F77_NAME(derivasplinessecondderiv)(double *,int *, int *, double *,int*, double *,double *,double *,
+                        int *,int *,int *,int *, int *, double *,double*, double *,
+                        int *,int *,int *,int *,int *,int *, double *,double*, double *, double *,int *,double *);
+
+ extern void F77_NAME(causalidmlikelihoodweib)(double *,int *, int *, double *,int*,
+                            int *,int *, double *,double*, double *,
+                            int *,int *,int *, int *,int*, int *,int *,int *, double *,double*, double *, double *,int *,double *);
+
+
 static const R_FortranMethodDef FortranEntries[] = {
     {"idmlikelihood",(DL_FUNC) &F77_NAME(idmlikelihood),    29},
     {"idmlikelihoodweib",(DL_FUNC) &F77_NAME(idmlikelihoodweib),    23},
+ {"causalidmlikelihoodweib",(DL_FUNC) &F77_NAME(causalidmlikelihoodweib),    24},
     {"derivaweib",(DL_FUNC) &F77_NAME(derivaweib),    22},
     {"derivaweiballpara",(DL_FUNC) &F77_NAME(derivaweiballpara),    22},
+ {"derivaweibsecondderiv",(DL_FUNC) &F77_NAME(derivaweibsecondderiv),    22},
     {"derivaweiballparadiag",(DL_FUNC) &F77_NAME(derivaweiballparadiag),    22},
     {"derivaweiballparafirstderiv",(DL_FUNC) &F77_NAME(derivaweiballparafirstderiv),    22},
+ {"derivaweibfirstderiv",(DL_FUNC) &F77_NAME(derivaweibfirstderiv),    22},
     {"derivaweibdiag",(DL_FUNC) &F77_NAME(derivaweibdiag),    22},
     {"firstderivaweib",(DL_FUNC) &F77_NAME(firstderivaweib),    22},
     {"derivaspline",(DL_FUNC) &F77_NAME(derivaspline),    28},
     {"derivasplinediag",(DL_FUNC) &F77_NAME(derivasplinediag),    28},
     {"firstderivaspline",(DL_FUNC) &F77_NAME(firstderivaspline),    28},
+  {"derivasplinesfirstderiv",(DL_FUNC) &F77_NAME(derivasplinesfirstderiv),    28},
+{"derivasplinessecondderiv",(DL_FUNC) &F77_NAME(derivasplinessecondderiv),    28},
     {NULL, NULL, 0}
 };
 
